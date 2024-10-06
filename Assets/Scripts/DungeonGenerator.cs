c436e0a955685552f728ca73ee1e3bbf9142d2da
SameGevDudios <magomed.chapaev1@gmail.com>
2024-10-06 14:01:54 +0300

    Add correct position spawn
     - Fix spawning tiles inside each other
    
     - Fix rotation bug
    
     - Add new material for tiles
    
     - Update tiles UV for new material
    
     - Update dungeon visualization. Now uses OnDrawGizmos() to draw used grid tiles
    
    Currently not a flawless version, there are two more bugs:
    
     - If tile hits dungeon borders, the generation stops
    
     - Soemtimes generaion stops if last spawned tile is EndingTile

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 public class DungeonGenerator : MonoBehaviour
 {
     [SerializeField] private int _dungeonSize, _tileScale;
-    private int previousSpawn = 1;
+    private int _previousSpawn = 0;
     private bool[,] _tileArray;
     [SerializeField] private GameObject[] _tile;
     [SerializeField] private GameObject _endingTile, _wall;
     private Stack<Tile> _tileQueue = new Stack<Tile>();
 
-    int spawnCount = 30, _currentX = 1, _currentY = 1;
+    int spawnCount = 30, _currentX, _currentY;
     [Header("Grid Visualization")]
     [SerializeField] private GameObject[] _row0, _row1, _row2;
     private void Start()
     {
         InitArray();
         SpawnCenter();
-        ActivateRowVisualizaiton();
+        SpawnNext();
     }
     private void InitArray()
     {
@@ -25,41 +25,40 @@ public class DungeonGenerator : MonoBehaviour
     }
     private void SpawnCenter()
     {
+        _currentX = _dungeonSize / 2;
+        _currentY = _currentX;
         Tile centerTile = Instantiate(_tile[0]).GetComponent<Tile>();
-        PlaceTile(centerTile, _dungeonSize / 2, _dungeonSize / 2, Quaternion.identity);
+        PlaceTile(centerTile, Quaternion.identity);
         _tileQueue.Push(centerTile);
-        ActivateRowVisualizaiton();
-        SpawnNext();
     }
     private void SpawnNext()
     {
         if(spawnCount > 0)
         {
-            print($"Tile queue count: {_tileQueue.Count}");
             if (_tileQueue.Count > 0)
             {
                 Tile previousTile = _tileQueue.Peek();
                 int spawn = previousTile.GetNextSpawn();
                 // calculate grid positon
-                float f = Mathf.PI * (spawn + previousSpawn - 1) / 2;
+                float f = Mathf.PI * spawn / 2 - Mathf.Deg2Rad * previousTile.transform.eulerAngles.y;
                 _currentX += (int)Mathf.Cos(f);
                 _currentY += (int)Mathf.Sin(f);
                 bool inRange = _currentX > -1 && _currentX < _dungeonSize && _currentY > -1 && _currentY < _dungeonSize;
-                print($"Function: {f}");
-                print($"Current spawn: {spawn}");
-                print($"previous spawn: {spawn}");
-                print($"x: {_currentX}, y: {_currentY}");
-                print($"In range: {inRange}");
                 if (spawn >= 0 && inRange)
                 {
+                    if (_tileArray[_currentX, _currentY])
+                    {
+                        SpawnNext();
+                        return;
+                    }
                     // calculate rotation of a new tile
                     Vector3 spawnEuler = Vector3.up * (spawn - 1) * -90 + previousTile.transform.localEulerAngles;
                     // randomly choose a new tile
                     int index = Random.Range(0, _tile.Length);
                     Tile newTile = Instantiate(_tile[index]).GetComponent<Tile>();
-                    PlaceTile(newTile, _currentX, _currentY, Quaternion.Euler(spawnEuler));
+                    PlaceTile(newTile, Quaternion.Euler(spawnEuler));
                     _tileQueue.Push(newTile);
-                    ActivateRowVisualizaiton();
+                    _previousSpawn = spawn;
                 }
                 else
                 {
@@ -69,28 +68,29 @@ public class DungeonGenerator : MonoBehaviour
                     _tileQueue.Pop();
                 }
                 SpawnNext();
-                previousSpawn = spawn;
                 spawnCount--;
             }
         }
 
     }
-    private void PlaceTile(Tile tile, int x, int y, Quaternion rotation)
+    private void PlaceTile(Tile tile, Quaternion rotation)
     {
-        tile.transform.position = (Vector3.right * x + Vector3.forward * y) * _tileScale;
+        tile.transform.position = (Vector3.right * _currentX + Vector3.forward * _currentY) * _tileScale;
         tile.transform.rotation = rotation;
-        _tileArray[x, y] = true;
+        _tileArray[_currentX, _currentY] = true;
     }
-    private void PlaceTile(Tile tile, Transform spawnTransform, int x, int y)
+    private void OnDrawGizmos()
     {
-        tile.transform.position = spawnTransform.position;
-        tile.transform.rotation = spawnTransform.rotation;
-        _tileArray[x, y] = tile;
-    }
-    private void ActivateRowVisualizaiton()
-    {
-        for (int i = 0; i < _row0.Length; i++) _row0[i].SetActive(_tileArray[0, i]);
-        for (int i = 0; i < _row1.Length; i++) _row1[i].SetActive(_tileArray[1, i]);
-        for (int i = 0; i < _row2.Length; i++) _row2[i].SetActive(_tileArray[2, i]);
+        if (!Application.isPlaying) return;
+        float verticalOffset = 5f;
+        float radius = .3f;
+        for (int i = 0; i < _dungeonSize; i++)
+        {
+            for (int j = 0; j < _dungeonSize; j++)
+            {
+                Gizmos.color = _tileArray[i, j] ? Color.green : Color.red;
+                Gizmos.DrawWireSphere(new Vector3(_tileScale * i, verticalOffset, _tileScale * j), radius);
+            }
+        }
     }
 }
